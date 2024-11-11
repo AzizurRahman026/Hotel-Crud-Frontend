2877fa9504a4f34876818aebf50fc1cf70ae2cff
Azizur Rahman <azizur.rahman@orbitax.com>
2024-11-11 18:39:17 +0600

    update hotels crud code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { HotelFormComponent } from './hotel-form/hotel-form.component';
 
 export const routes: Routes = [
-    // {path: '', component: AppComponent},
-    // {path: 'gethotels', component: AppComponent},
-    // {path: '**', component: AppComponent, pathMatch: 'full'}
+    { path: '', component: AppComponent },
+    { path: 'add', component: HotelListComponent },
+    { path: 'edit/:id', component: HotelFormComponent },
 ];
